81a07d5966e464445a9129bbe9f9763a1805d32b
John Dilger <johnjdilger@gmail.com>
2024-04-17 00:51:36 -0500

    add more indices, use combine for reducers

--- processing.py
+++ processing.py
@@ -13,7 +13,7 @@ ee.Initialize(
 )
 
 input_file = "data/NEON_metadata.csv"
-output_file = "data/NEON_metadata_ee.csv"
+output_file = "data/NEON_metadata_ee_2.csv"
 start_date, end_date = "2018-01-01", "2018-12-31"
 
 points = pd.read_csv(input_file)
@@ -42,16 +42,34 @@ QA_BAND = "cs_cdf"
 # Higher values will remove thin clouds, haze & cirrus shadows.
 CLEAR_THRESHOLD = 0.50
 
-# NDMI = (NIR - SWIR1)/(NIR + SWIR1)
+
+def indices(img: ee.Image) -> ee.Image:
+    img = img.multiply(0.0001)
+    ndmi = img.normalizedDifference(["B8", "B11"]).rename("NDMI")
+    ndvi = img.normalizedDifference(["B8", "B4"]).rename("NDVI")
+    nbr = img.normalizedDifference(["B8", "B12"]).rename("NBR")
+    nd_gr = img.normalizedDifference(["B3", "B4"]).rename("ND_GREEN_RED")
+    red_swir_r = img.select("B8").divide(img.select("B11")).rename("RED_SWIR_RATIO")
+    savi = img.expression(
+        "((NIR - RED) / (NIR + RED + 0.5)) * (1 + 0.5)",
+        {"NIR": img.select("B8"), "RED": img.select("B4")},
+    ).rename("SAVI")
+    return ee.Image.cat([ndmi, ndvi, nbr, red_swir_r, nd_gr, savi])
+
 
 collection = (
     s2.filterDate(start_date, end_date)
     .linkCollection(csPlus, [QA_BAND])
     .map(lambda img: img.updateMask(img.select(QA_BAND).gte(CLEAR_THRESHOLD)))
-    .map(lambda img: img.normalizedDifference(["B8", "B11"]).rename("NDMI"))
+    .map(lambda img: indices(img))
 )
-reducers = ee.Reducer.percentile([10, 90]).combine(
-    reducer2=ee.Reducer.mean(), sharedInputs=True
+reducers = (
+    ee.Reducer.percentile([10, 90])
+    .combine(
+        reducer2=ee.Reducer.mean(),
+        sharedInputs=True,
+    )
+    .combine(reducer2=ee.Reducer.stdDev(), sharedInputs=True)
 )
 image = collection.reduce(reducers)
 new_feats = image.reduceRegions(
